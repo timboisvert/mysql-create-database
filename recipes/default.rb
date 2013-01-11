c91a6da8a0f55c5a0861f63c4e88a880321cd0bf
Tim Boisvert <tboisvert@asheavenue.com>
2013-01-11 15:24:04 -0500

    Updated to support multiple databases

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
-db_names = node[:mysql_create_database][:db_name]
-db.names.each do |db_name|
-  bash "Creating database #{node[:mysql_create_database][:db_name]}" do
+db_names = node[:mysql_create_database][:db_names]
+db_names.each do |db_name|
+  bash "Creating database #{db_name}" do
     not_if("/usr/bin/mysql -uroot -p#{node[:mysql][:server_root_password]} -e'show databases' | grep #{db_name}", :user => 'root')
     user "root"
     code <<-EOM
